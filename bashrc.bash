74077edb7bf68d6163e368e49de44107043a9ea3
jashanpreet singh <jashan@pop-os.localdomain>
2021-05-18 20:15:17 +0530

    Added function to force enable adb with authorization.

--- bashrc.bash
+++ bashrc.bash
@@ -52,3 +52,20 @@ make_kernel_zip() {
         cd -
 }
 
+enable_adb() { 
+        adb shell mount /system
+        adb shell
+
+        echo "persist.service.adb.enable=1" >> default.prop 
+        echo "persist.service.debuggable=1" >> default.prop
+        echo "persist.sys.usb.config=mtp,adb" >> default.prop
+        echo "persist.service.adb.enable=1" >> /system/build.prop 
+        echo "persist.service.debuggable=1" >> /system/build.prop
+        echo "persist.sys.usb.config=mtp,adb" >> /system/build.prop
+        exit
+        path=$(pwd)
+        cd ~/.android
+        adb push adbkey.pub /data/misc/adb/adb_keys
+        cd $path
+}
+
